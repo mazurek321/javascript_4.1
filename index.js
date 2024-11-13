708f4165f9579cbae45a1816853b48bd5b97960e
BartekM <160483027+mazurek321@users.noreply.github.com>
2024-11-13 10:51:27 +0000

    initialX

--- index.js
+++ index.js
@@ -49,6 +49,64 @@ app.get("/math/power/:base/:exponent", (req, res) => {
 });
 //TODO3
 
+let categories = ["funnyJoke", "lameJoke"];
+
+let funnyJoke = [
+  {
+    joke: "Dlaczego komputer poszedł do lekarza?",
+
+    response: "Bo złapał wirusa!",
+  },
+
+  {
+    joke: "Dlaczego komputer nie może być głodny?",
+
+    response: "Bo ma pełen dysk!",
+  },
+
+  {
+    joke: "Co mówi jeden bit do drugiego?",
+
+    response: "„Trzymaj się, zaraz się przestawiamy!”",
+  },
+];
+
+let lameJoke = [
+  {
+    joke: "Dlaczego programiści preferują noc?",
+
+    response: "Bo w nocy jest mniej bugów do łapania!",
+  },
+
+  {
+    joke: "Jak nazywa się bardzo szybki programista?",
+
+    response: "Błyskawiczny kompilator!",
+  },
+];
+
+app.get("/jokebook/categories", (req, res) => {
+  res.json(categories);
+});
+
+app.get("/jokebook/joke", (req, res) => {
+  const category = req.query.category;
+  if (!category) {
+    return res.status(400);
+  }
+  if (category === "funnyJoke") {
+    const randomJoke = funnyJoke[Math.floor(Math.random() * funnyJoke.length)];
+    res.json(randomJoke);
+  } else {
+    const randomJoke = lameJoke[Math.floor(Math.random() * lameJoke.length)];
+    res.json(randomJoke);
+  }
+  // res.json(categories);
+});
+
+
+
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
